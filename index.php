ce91478a2a85356c51e2d7d78337ec2e3530ec6c
1149141 <57629957+1149141@users.noreply.github.com>
2019-11-19 10:11:01 +0100

    index

--- index.php
+++ index.php
@@ -7,22 +7,34 @@ include 'ProductFuncties.php';
 <head>
     <meta charset="UTF-8">
     <title>Wide World Importers!</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<h1>Product pagina</h1>
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="">News</a></li>
+    <li><a href="">Contact</a></li>
+    <li><a href="">About</a></li>
+</ul>
 
-    <h1>Producten</h1>
-<br>
-    <form action="index.php" method="get">
-        Zoekterm :<input type="text" value="<?php if (isset($_GET['term'])) echo $_GET['term']; ?>" name="term">
-        <input type="submit" value="Submit"> <br>
-    </form>
+<container>
+    <br>
+    <div>
+        <form action="index.php" method="get">
+            <input type="text" name="term">
+            <input type="submit" value="Zoek" class="btn btn-primary">
+        </form>
+    </div>
+    <br>
     <?php
     if(isset($_GET["term"]) == true){
         $producten = GezochteProductenOpVragen($_GET["term"]);
     }       else
-        {$producten = AlleProductenOpVragen();} ?>
-    <?php ToonProductenOpScherm($producten); ?>
+    {$producten = AlleProductenOpVragen();} ?>
+    <br>
 
+    <?php ToonProductenOpScherm($producten); ?>
+</container>
 </body>
 </html>
