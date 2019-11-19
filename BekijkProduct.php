a6d1053171d685e8ddd1b7ea4c1e4e8b3a055cf5
1145157 <57630294+1145157@users.noreply.github.com>
2019-11-19 15:39:16 +0100

    Title bekijkpagina is productnaam

--- BekijkProduct.php
+++ BekijkProduct.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>$Title$</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-</head>
-<body>
-
 <?php
 
 include "ProductFuncties.php";
@@ -17,6 +6,17 @@ $gegevens["StockItemID"] = isset($_GET["StockItemID"]) ? $_GET["StockItemID"] : 
 $gegevens = ProductGegevensOpvragen($gegevens);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php print($gegevens['StockItemName']); ?></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
 
 <ul>
     <li><a href="index.php">Home</a></li>
@@ -37,9 +37,6 @@ print($gegevens["StockItemName"]);
        print($gegevens["unitPrice"] . "<br>"); ?>
     <a href="index.php"><button type="button" class="btn btn-primary">Verder zoeken</button></a>
 </div>
-
-
-
 
 <br><?php print($gegevens["melding"]); ?><br>
 
