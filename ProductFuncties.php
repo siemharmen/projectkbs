a668654483b181eafd94e4e820f5a78235e3ac75
1145157 <57630294+1145157@users.noreply.github.com>
2019-11-20 13:31:03 +0100

    productfoto frontpage

--- ProductFuncties.php
+++ ProductFuncties.php
@@ -46,6 +46,7 @@ function ToonGoedkoopProductenOpScherm($goedkoopProducten)
     foreach ($goedkoopProducten as $gproduct) {
 
         print("<div class='col-4'><div class='center'>");
+        print("<img src='https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?v=1530129458' style='width: 50%'> <br> ");
         print($gproduct["StockItemName"] . "<br>");
         print($gproduct["unitPrice"] . "<br>");
         print("<a href=\"BekijkProduct.php?StockItemID=" . $gproduct["StockItemID"] . "\"><button type=\"button\" class=\"btn btn-primary btn-sm\">Bekijk</button></a><br><br> ");
