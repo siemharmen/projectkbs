8ac4d7231d34501f8142c6b5c259c92fbf03b94c
1145157 <57630294+1145157@users.noreply.github.com>
2019-11-20 11:23:59 +0100

    frontpage foto
    css styles.css genoemd omdat niet meer werkte

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Wide World Importers!</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -18,7 +18,9 @@
 <h1>Wide World Importers! </h1>
 
 
-
+<div class="frontpageimage">
+    <img src="https://vitanovamedicalcentre.nl/wp-content/uploads/2016/11/placeholder-36-1170x500.jpg">
+</div>
 
 
 
