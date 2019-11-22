2c5ed556e2427eeef5d70f8e9f2eb8b68320d5d3
1142387 <57629987+1142387@users.noreply.github.com>
2019-11-22 11:48:52 +0100

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
 <li><a href="index.php">Home</a></li>
 <li><a href="ProductPagina.php">Producten</a></li>
-<li><a href="">Contact</a></li>
-<li><a href="">About</a></li>
+<!--  <li><a href="">Contact</a></li>
+<li><a href="">About</a></li> -->
