49745aefb0525de3c7b8f2bcd09ae80988ddc3c0
1142387 <57629987+1142387@users.noreply.github.com>
2019-11-22 12:00:24 +0100

    fav

--- ProductPagina.php
+++ ProductPagina.php
@@ -9,6 +9,7 @@ include 'ProductFuncties.php';
     <title>Wide World Importers!</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/x-icon" href="wide.png" />
 </head>
 <body>
 <!-- navigatiebalk -->
