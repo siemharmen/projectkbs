4ce5b21cc8fb4b740caec521833b8d0e4b8a372c
1142387 <57629987+1142387@users.noreply.github.com>
2019-11-26 14:19:19 +0100

    werkendelogin

--- loginserv.php
+++ loginserv.php
@@ -1,13 +1,13 @@
 <?php
 $error=''; //Variable to Store error message;
 if(isset($_POST['submit'])){
-    if(empty($_POST['user']) || empty($_POST['pass'])){
+    if(empty($_POST['User']) || empty($_POST['Pass'])){
         $error = "Username or Password is Invalid";
     }
     else
     {
-        $user=$_POST['user'];
-        $pass=$_POST['pass'];
+        $user=$_POST['User'];
+        $pass=$_POST['Pass'];
         $conn = mysqli_connect("localhost", "root", "");
         $db = mysqli_select_db($conn, "widewordimporters");
         $query = mysqli_query($conn, "SELECT * FROM loginform WHERE Pass='$pass' AND User='$user'");
