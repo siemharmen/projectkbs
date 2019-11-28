dc1f4e59aac615a6b022f50c1f9aa263beb6c41b
1142387 <57629987+1142387@users.noreply.github.com>
2019-11-28 15:01:12 +0100

    niet inloggen

--- navbar.php
+++ navbar.php
@@ -45,7 +45,7 @@ $gegevens = ProductGegevensOpvragen($gegevens);
      <?php /* if (isset($_SESSION['username'])) : ?>
         <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
         <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-    <?php endif*/ ?>  
+    <?php endif*/ ?>
 
 </nav>
 
