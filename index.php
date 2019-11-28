a0a703bffd88ded4327d44fdf1beec3c70def1f2
1145157 <57630294+1145157@users.noreply.github.com>
2019-11-28 14:29:40 +0100

    niks aan de hand sem

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ include 'ProductFuncties.php';
 
 ?>
 <?php
-//session_start();
-//
-//if (!isset($_SESSION['username'])) {
-//    $_SESSION['msg'] = "You must log in first";
-//    header('location: loginpagina.php');
-//}
-//if (isset($_GET['logout'])) {
-//    session_destroy();
-//    unset($_SESSION['username']);
-//    header("location: loginpagina.php");
-//}
-//?>
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: loginpagina.php');
+}
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: loginpagina.php");
+}
+?>
 
 
 <!-- navigatiebalk -->
