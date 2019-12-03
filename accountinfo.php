43e7bca9412a60fb3e1b311cd57126177707bedd
1142387 <57629987+1142387@users.noreply.github.com>
2019-12-03 11:37:14 +0100

    updateaccinfo

--- accountinfo.php
+++ accountinfo.php
@@ -1,1 +1,15 @@
-<?php
+<?php include 'ProductFuncties.php';?>
+<?php include 'server.php';?>
+<html>
+<head>
+    <title>Registreren</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/x-icon" href="wide.png" />
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+
+</body>
+</html>
+
