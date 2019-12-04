0866209bc607c54845319517c7d7666d009a31b6
1142387 <57629987+1142387@users.noreply.github.com>
2019-12-04 14:42:13 +0100

    -geel

--- registreerpagina.php
+++ registreerpagina.php
@@ -4,10 +4,6 @@ include 'ProductFuncties.php';
 ?>
 <html>
 <head>
-    <title>Registreren</title>
-    <link rel="stylesheet" href="styles1.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="shortcut icon" type="image/x-icon" href="wide.png" />
 </head>
 <body>
 <?php include 'navbar.php'; ?>
