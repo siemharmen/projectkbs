0fb2d1817f5277e5163dbeae27b908e7669af421
1149141 <57629957+1149141@users.noreply.github.com>
2019-12-12 10:19:29 +0100

    Update CustomerServicePage.php

--- CustomerServicePage.php
+++ CustomerServicePage.php
@@ -7,7 +7,7 @@
         <h2>Klantenservice</h2>
         <h3>Snel antwoord op uw vraag!</h3>
 
-        <body>Wanneer u een vraag heeft mail dan naar klantenservice@wwi.com of neem telefonisch contact op via 0612345678</body>
+        <body> <br> Wanneer u een vraag heeft mail dan naar klantenservice@wwi.com of neem telefonisch contact op via 0612345678</body>
 
 
 
